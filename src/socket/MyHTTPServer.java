0c9bf06b22343c988917fe9071cfd528a8c8d77e
Coco <chatchai@geniustree.co.th>
2019-09-03 07:42:49 +0700

    .

--- src/socket/MyHTTPServer.java
+++ src/socket/MyHTTPServer.java
@@ -1,6 +1,5 @@
 package socket;
 
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,42 +7,68 @@ import java.net.Socket;
 public class MyHTTPServer {
     public static void main(String[] args) throws IOException {
         System.out.println("connection port 5000 .....");
+        ServerSocket serve = null;
+        PrintWriter out = null;
+        Socket s = null;
+        BufferedReader br = null;
+        BufferedOutputStream or = null;
+        try {
+            serve = new ServerSocket(5000);
+            s = serve.accept();
+            out = new PrintWriter(s.getOutputStream());
+            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            or = new BufferedOutputStream(s.getOutputStream());
+            String input = br.readLine();
+            String[] split = input.split(" ");
 
-        try (ServerSocket serve = new ServerSocket(5000);
-             Socket s = serve.accept();
-             BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-             BufferedOutputStream or = new BufferedOutputStream(s.getOutputStream());
-        ) {
-            PrintWriter out = new PrintWriter(s.getOutputStream());
+            String filename = split[1];
+            File file = new File(".", filename);
 
-            while (true) {
-                String input = br.readLine();
-                System.out.println(input);
-                File file = new File("index.html");
-                int fileLength = (int) file.length();
-                if (input.contains(file.getName())) {
-                    byte[] fileData = readFile(file, fileLength);
-                    out.println("HTTP/1.1 200 is  OK");
-                    out.println();
-                    out.flush();
-                    or.write(fileData, 0, fileLength);
-                    or.flush();
-                }
-
+            int fileLength = (int) file.length();
+            if (input.contains("GET")) {
+                byte[] fileData = readFile(file, fileLength);
+                out.println("GET");
+                out.println("HTTP/1.1 200 OK");
+                out.println();
+                out.flush();
+                or.write(fileData, 0, fileLength);
+                or.flush();
             }
-        }
+        } catch (FileNotFoundException fn) {
+            try {
+                String notfound = "fileNotFound.html";
+                File filenotFound = new File(".", notfound);
+                int fileLength = (int) filenotFound.length();
 
+                byte[] fileData = readFile(filenotFound, fileLength);
+                out.println("GET");
+                out.println("HTTP/1.1 400 FILE NOT FOUND");
+                out.println();
+                out.flush();
+                or.write(fileData, 0, fileLength);
+                or.flush();
+            } catch (IOException ioe) {
+                System.out.println(" Error Exception " + ioe.getMessage());
+            }
+
+        } finally {
+            out.close();
+            br.close();
+            or.close();
+
+        }
 
     }
 
 
     private static byte[] readFile(File file, int fileLength) throws IOException {
         byte[] fileData = new byte[fileLength];
-        FileInputStream fileIn = new FileInputStream(file);
-        int f = fileIn.read(fileData);
-        fileIn.read(fileData);
-        while (f == -1) {
-            break;
+        try (InputStream fileIn = new FileInputStream(file)) {
+            int f = fileIn.read(fileData);
+            while (f == -1) {
+                break;
+            }
+
         }
         return fileData;
     }
